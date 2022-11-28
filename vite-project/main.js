9aa9c61e808aaa8696fa1aae359159b9c334b18c
Christiano0407 <placebo0704@gmail.com>
2022-11-28 16:07:17 -0600

    add list

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,3 +1,16 @@
 //import './style.css';
 //** === === Movie App === === */
 import { API_KEY, Token } from './src/secret/secret.js';
+
+const navAdd = document.querySelector(`#idNavAdd`);
+const idNavBtn = document.querySelector('#idNavBtn');
+
+const addNavigation = () => {
+  console.log('Click');
+  /* 
+  if (navAdd === 'inactive') {
+    navAdd.classList.add('active');
+  } */
+};
+
+idNavBtn.addEventListener('click', addNavigation);
