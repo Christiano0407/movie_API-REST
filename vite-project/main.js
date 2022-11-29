f13e4f812afccd67e9e6b91844a44524b2aef580
Christiano0407 <placebo0704@gmail.com>
2022-11-29 09:40:45 -0600

    add new request and section mobile

--- vite-project/main.js
+++ vite-project/main.js
@@ -2,15 +2,18 @@
 //** === === Movie App === === */
 import { API_KEY, Token } from './src/secret/secret.js';
 
-const navAdd = document.querySelector(`#idNavAdd`);
+const navigationAdd = document.querySelector(`#idNavAdd`);
 const idNavBtn = document.querySelector('#idNavBtn');
+const iconBtn = document.querySelector(`#iconBtn`);
 
 const addNavigation = () => {
   console.log('Click');
-  /* 
-  if (navAdd === 'inactive') {
-    navAdd.classList.add('active');
-  } */
+
+  if (navigationAdd) {
+    /* navigationAdd.classList.remove(`inactive`);
+    navigationAdd.classList.add(`active`); */
+    navigationAdd.classList.toggle(`active`);
+  }
 };
 
 idNavBtn.addEventListener('click', addNavigation);
