9d18d2c40b2803ea1d6e6d1d6f64717d6cb5bf3a
Christiano0407 <placebo0704@gmail.com>
2022-12-13 15:59:19 -0600

    new search API

--- vite-project/src/js/search.js
+++ vite-project/src/js/search.js
@@ -1,3 +1,7 @@
+//** === Import Key === */
+import { API_KEY } from '../secret/secret.js';
+//** >= === API REST Search === <= */
+const API_SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}`;
 //** === Variables ===  */
 const headerForm = document.querySelector(`#idHeaderForm`);
 const searchFormInput = document.querySelector(`#searchForm`);
@@ -7,9 +11,15 @@ const navigation = document.querySelector(`#idNav`);
 const categories = document.querySelector(`#categoriesPreview`);
 const trending = document.querySelector(`#trendingPreview`);
 const tvShow = document.querySelector(`#idSectionTv`);
+const navBtn = document.querySelector(`#idNavBtn`);
+const arrow = document.querySelector(`.arrow`);
+const arrowHeader = document.querySelector(`.header-arrow`);
 
 const noneSearch = () => {
-  navigation.style.display = `none`;
+  navigation.style.display = `flex`;
+  navBtn.style.display = `none`;
+  arrow.style.display = `flex`;
+  arrowHeader.style.display = `flex`;
   categories.style.display = `none`;
   trending.style.display = 'none';
   tvShow.style.display = `none`;
@@ -21,3 +31,9 @@ export const searchPlay = () => {
 };
 
 searchBtn.addEventListener(`click`, searchPlay);
+
+const returnSearch = () => {
+  arrow.style.display = `none`;
+};
+
+arrowHeader.addEventListener(`click`, returnSearch);
